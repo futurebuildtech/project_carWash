7dd8094651b7786c8cedcbf4143336de84a8d96b
krishnarjun2108 <krishnarjun2108@gmail.com>
2026-09-15 18:49:53 +0530

    updated aboutr.tsx

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
+ import { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 // import Header from '@/components/Header';
 import { useUserStore, User } from '@/lib/store';
